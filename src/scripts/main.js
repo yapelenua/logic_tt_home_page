04c7aaeca56bd18e69ecb549e62a03f00aa4d2eb
Yaroslav <kbshnik2020@gmail.com>
2023-07-17 02:20:46 +0300

    test task finished

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,7 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('burger').addEventListener('click', function() {
+    document.querySelector('header').classList.toggle('open');
+  });
+});
